c559bbbaa9c50c26b925f65ce48b35f8f3cd5264
12Me21 <12Me21.mc@gmail.com>
2023-04-28 21:06:14 -0400

    test

---
+++ ps.js
@@ -0,0 +1,391 @@
+let token = /%.*|\[|\]|[/].*?}|[^\s\[\]]+/g
+let text = `
+36 36 pdfSetupPaper
+pdfStartPage
+0 0 36 36 re W
+%%EndPageSetup
+[] 0 d
+1 i
+0 j
+0 J
+10 M
+1 w
+/DeviceGray {} cs
+[0] sc
+/DeviceGray {} CS
+[0] SC
+false op
+false OP
+{} settransfer
+q
+q
+0 0 36 36 re
+W
+/DeviceRGB {} cs
+[0.6 0.667 0.71] sc
+false op
+false OP
+q
+[1 0 0 1 18.04 22.0503] cm
+0 0 m
+-18.04 -12.05 l
+-18.04 -14.05 l
+-18.04 -20.05 l
+-18.04 -21.155 -17.145 -22.05 -16.04 -22.05 c
+15.96 -22.05 l
+17.064 -22.05 17.96 -21.155 17.96 -20.05 c
+17.96 -14.05 l
+17.96 -12.061 l
+0 0 l
+h
+f
+Q
+/DeviceRGB {} cs
+[0.4 0.459 0.498] sc
+0 2 36 1 re
+f
+0 5 36 1 re
+f
+0 8 36 1 re
+f
+q
+[1 0 0 1 1.4971 11] cm
+0 0 m
+32.998 0 l
+31.509 1 l
+1.497 1 l
+0 0 l
+h
+f
+Q
+/DeviceRGB {} cs
+[0.882 0.91 0.929] sc
+q
+[1 0 0 1 18 36] cm
+0 0 m
+-7.18 0 -13 -1.343 -13 -3 c
+-13 -36 l
+13 -36 l
+13 -3 l
+13 -1.343 7.18 0 0 0 c
+f
+Q
+/DeviceRGB {} cs
+[0.8 0.839 0.867] sc
+q
+[1 0 0 1 18 23] cm
+0 0 m
+7.18 0 13 -1.344 13 -3 c
+13 -1 l
+13 0.657 7.18 2 0 2 c
+-7.18 2 -13 0.657 -13 -1 c
+-13 -3 l
+-13 -1.344 -7.18 0 0 0 c
+f
+Q
+q
+[1 0 0 1 18 11] cm
+0 0 m
+7.18 0 13 -1.344 13 -3 c
+13 -1 l
+13 0.656 7.18 2 0 2 c
+-7.18 2 -13 0.656 -13 -1 c
+-13 -3 l
+-13 -1.344 -7.18 0 0 0 c
+f
+Q
+q
+[1 0 0 1 18 5] cm
+0 0 m
+7.18 0 13 -1.344 13 -3 c
+13 -1 l
+13 0.656 7.18 2 0 2 c
+-7.18 2 -13 0.656 -13 -1 c
+-13 -3 l
+-13 -1.344 -7.18 0 0 0 c
+f
+Q
+q
+[1 0 0 1 18 29] cm
+0 0 m
+7.18 0 13 -1.344 13 -3 c
+13 -1 l
+13 0.657 7.18 2 0 2 c
+-7.18 2 -13 0.657 -13 -1 c
+-13 -3 l
+-13 -1.344 -7.18 0 0 0 c
+f
+Q
+q
+[1 0 0 1 14 35.8535] cm
+0 0 m
+0 -35.854 l
+2 -35.854 l
+2 0.107 l
+1.315 0.083 0.646 0.048 0 0 c
+f
+Q
+q
+[1 0 0 1 8 34.916] cm
+0 0 m
+0 -34.916 l
+2 -34.916 l
+2 0.445 l
+1.268 0.313 0.594 0.165 0 0 c
+f
+Q
+q
+[1 0 0 1 20 35.9609] cm
+0 0 m
+0 -35.961 l
+2 -35.961 l
+2 -0.107 l
+1.354 -0.059 0.685 -0.024 0 0 c
+f
+Q
+q
+[1 0 0 1 26 35.3613] cm
+0 0 m
+0 -35.361 l
+2 -35.361 l
+2 -0.445 l
+1.406 -0.28 0.731 -0.132 0 0 c
+f
+Q
+/DeviceRGB {} cs
+[0.161 0.184 0.2] sc
+q
+[1 0 0 1 18 17] cm
+0 0 m
+7.18 0 13 -1.344 13 -3 c
+13 -1 l
+13 0.657 7.18 2 0 2 c
+-7.18 2 -13 0.657 -13 -1 c
+-13 -3 l
+-13 -1.344 -7.18 0 0 0 c
+f
+Q
+/DeviceRGB {} cs
+[0.745 0.098 0.192] sc
+q
+[1 0 0 1 11.1299 30.4175] cm
+0 0 m
+-0.708 0 l
+-1.284 0 -1.523 0.42 -1.523 0.828 c
+-1.523 1.247 -1.224 1.655 -0.708 1.655 c
+0.995 1.655 l
+1.511 1.655 1.799 1.283 1.799 0.804 c
+1.799 -6.021 l
+1.799 -6.621 1.415 -6.956 0.899 -6.956 c
+0.384 -6.956 0 -6.621 0 -6.021 c
+0 0 l
+h
+f
+Q
+q
+[1 0 0 1 18.4951 28.2695] cm
+0 0 m
+0 0.912 -0.084 2.783 -1.307 2.783 c
+-2.53 2.783 -2.614 0.912 -2.614 0 c
+-2.614 -0.852 -2.53 -2.782 -1.307 -2.782 c
+-0.084 -2.782 0 -0.852 0 0 c
+-4.485 0 m
+-4.485 1.955 -3.729 4.414 -1.307 4.414 c
+1.116 4.414 1.871 1.955 1.871 0 c
+1.871 -1.955 1.116 -4.414 -1.307 -4.414 c
+-3.729 -4.414 -4.485 -1.955 -4.485 0 c
+f
+Q
+q
+[1 0 0 1 25.2158 29.2422] cm
+0 0 m
+0 0.671 -0.419 1.247 -1.139 1.247 c
+-1.822 1.247 -2.278 0.695 -2.278 0 c
+-2.278 -0.636 -1.895 -1.248 -1.139 -1.248 c
+-0.455 -1.248 0 -0.708 0 0 c
+1.871 0 m
+1.871 -1.811 0.036 -4.354 -1.175 -5.481 c
+-1.271 -5.577 -1.415 -5.673 -1.559 -5.769 c
+-1.69 -5.853 -1.822 -5.853 -1.919 -5.853 c
+-2.254 -5.853 -2.771 -5.409 -2.771 -4.978 c
+-2.771 -4.81 -2.662 -4.666 -2.519 -4.486 c
+-2.038 -3.898 -1.259 -3.13 -0.863 -2.567 c
+-0.887 -2.543 l
+-1.103 -2.639 -1.379 -2.687 -1.643 -2.687 c
+-3.082 -2.687 -4.149 -1.416 -4.149 0 c
+-4.149 1.559 -2.914 2.974 -1.139 2.974 c
+0.708 2.974 1.871 1.595 1.871 0 c
+f
+Q
+Q
+Q
+`
+
+function assert(cond, msg) {
+	if (!cond)
+		throw new Error(msg)
+}
+
+let stack0 = []
+let array
+let stack = stack0
+
+let pop = ()=>stack.pop()
+let pops = n=>stack.splice(-n,n)
+let push = x=>stack.push(x)
+
+class Elem {
+	path = ""
+	fill = null
+	transform = []
+	children = []
+	toString() {
+		let out = ""
+		//if (this.children) {
+		out += `<g`
+		//}
+		if (this.fill)
+			out += ` fill="${this.fill}"`
+		if (this.transform.length)
+			out += ` transform="${this.transform.join(" ")}"`
+		
+		out += ">"
+		if (this.path)
+			out += `<path d="${this.path}"/>`
+		out += this.children.join("\n")
+		out += "</g>"
+		return out
+	}
+}
+
+let elem = new Elem()
+let root = new Elem()
+root.children.push(elem)
+let estack = [root]
+
+for (let [x] of text.matchAll(token)) {
+	if (x.startsWith("%"))
+		continue
+	let num = +x
+	if (!isNaN(x)) {
+		stack.push(x)
+		continue
+	}
+	if (x=='false') {
+		stack.push(false)
+		continue
+	}
+	if (x=='true') {
+		stack.push(true)
+		continue
+	}
+	if (x=='{}') {
+		stack.push(x)
+		continue
+	}
+	
+	let f = {
+		'['() {
+			stack = array = []
+		},
+		']'() {
+			stack = stack0
+			stack.push(array)
+		},
+		'h'() { elem.path += "Z" },
+		'f'() { 'fill' },
+		'q'() {
+			let nw = new Elem()
+			elem.children.push(nw)
+			estack.push(elem)
+			elem = nw
+		},
+		'Q'() {
+			elem = estack.pop()
+		},
+		'sc'() {
+			if (elem.path || elem.fill) {
+				let nw = new Elem()
+				estack[estack.length-1].children.push(nw)
+				elem = nw
+			}
+			let col = pop()
+			if (col.length==1 && col[1]=="0")
+				col = "none"
+			else
+				col = "rgb("+pop().map(x=>Math.round(x*255))+")"
+			elem.fill = col
+		},
+		'SC'() {
+			elem.stroke = pop()
+		},
+		'cm'() {
+			elem.transform.push("matrix("+pop().join(" ")+")")
+		},
+		'm'() {
+			elem.path += "M "+pops(2)
+		},
+		'l'() {
+			elem.path += "L "+pops(2)
+		},
+		'c'() {
+			elem.path += "C "+pops(6)
+		},
+		're'() {
+			let [x,y,w,h] = pops(4)
+			elem.path += "M "+[x,y]+" h "+w+" v "+h+" h "+-w+" v "+-h+" Z"
+		},
+		'W'() { 'set clip path' },
+		'w'() {
+			pop() // stroke width
+		},
+		'd'() {
+			assert(pop()==0,'d')
+			assert(pop().length==0,'d')
+		},
+		'i'() {
+			assert(pop()==1, 'i')
+		},
+		'j'() {
+			assert(pop()==0, 'j')
+		},
+		'J'() {
+			assert(pop()==0, 'J')	
+		},
+		'M'() {
+			pop() // miter clip
+		},
+		
+		'settransfer'() {
+			assert(pop()=="{}", 'settransfer')
+		},
+		'pdfStartPage'() { },
+		
+		'/DeviceRGB {}'() { },
+		'/DeviceGray {}'() { },
+		
+		'pdfSetupPaper'() {
+			let [w,h] = pops(2)
+			console.log(`viewBox="0 0 ${w} ${h}"`)
+		},
+		
+		'cs'() { 'fill colorspace' },
+		'CS'() { 'stroke colorspace' },
+		'op'() {
+			assert(pop()==false, 'op')
+		},
+		'OP'() {
+			assert(pop()==false, 'OP')
+		},
+		
+	}[x]
+	if (!f)
+		console.warn('what is', x)
+	else
+		f()
+}
+
+console.log(String(elem))
+
+if (stack.length)
+	console.warn('stack not empty')//, stack)
