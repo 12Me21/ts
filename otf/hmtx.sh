7c2471f50f48763c04d29257389c744b6ad0d169
12Me21 <12Me21.mc@gmail.com>
2023-08-12 15:27:59 -0400

    sfdir

--- otf/hmtx.sh
+++ otf/hmtx.sh
@@ -1,6 +1,6 @@
 #ttx -f -o hmtx-raw.ttx -t hmtx test2.ttf
 #node hmtx.js hmtx-raw.ttx >hmtx.ttx
 
-ttx -v -b -m test2.ttf -o test2-colr.ttf colr.xml 
+ttx -v -b -m raw.ttf -o faces-colr.ttf colr.xml
 
 # TODO: remove unnecessary tables like GDEF etc.
