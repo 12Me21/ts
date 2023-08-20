dc7d95e26490b43bcd3b186ec886c2f06beec474
12Me21 <12Me21.mc@gmail.com>
2023-08-20 12:17:42 -0400

    damn

--- otf/hmtx.sh
+++ otf/hmtx.sh
@@ -3,6 +3,8 @@
 
 ttx -v -b -m raw.ttf -o faces-colr.ttf colr.xml
 
-fonttools subset faces-colr.ttf '*' --no-subset-tables+=COLR,CPAL --canonical-order --drop-tables+=gasp,BASE,GDEF,GPOS --flavor=woff2
+fonttools subset faces-colr.ttf '*' --no-subset-tables+=COLR,CPAL --canonical-order --no-legacy-cmap --drop-tables+=gasp,BASE,GDEF,GPOS --flavor=woff2
+
+#todo: why is it keeping the legacy cmap! fuck!
 
 # hi
